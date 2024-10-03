a80d018dd69350bb1c8546e73cf28e90b275daac
Tom Zhang <tommyawesome08@gmail.com>
2024-10-03 12:26:06 -0400

    added DRF

--- verinomics/verinomics/settings.py
+++ verinomics/verinomics/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'django_ckeditor_5',
     'ckeditor',
     'quiz',
