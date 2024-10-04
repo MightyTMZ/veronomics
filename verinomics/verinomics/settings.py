f738243379eaf1ff1a6174b8318750bc26583a3d
Tom Zhang <tommyawesome08@gmail.com>
2024-10-03 22:34:08 -0400

    quiz functionality works now

--- verinomics/verinomics/settings.py
+++ verinomics/verinomics/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'django_ckeditor_5',
     'ckeditor',
     'quiz',
@@ -50,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -58,6 +60,10 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'verinomics.urls'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173'
+]
 
 TEMPLATES = [
     {
